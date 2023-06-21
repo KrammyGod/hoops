6c6e65466c0bedfa52395cc59e619253c8d03488
Brandon Ngo <okibrandon@hotmail.com>
2023-06-21 14:58:22 -0400

    r9 done

--- sql/r9a.sql
+++ sql/r9a.sql
@@ -1,8 +1,11 @@
 /* INSERT PLAYERS INTO WATCHLIST */
 INSERT INTO bookmarks VALUES (..., ...);
 
-/* GET PLAYERS FROM WATCHLIST */
-SELECT * FROM bookmarks JOIN player WHERE player.pid = ...;
+/* GET SINGLE PLAYERS FROM WATCHLIST */
+SELECT * FROM bookmarks, player WHERE player.pid = ... and bookmarks.pid = player.pid;
+
+/* GET ALL PLAYERS FROM WATCHLIST */
+SELECT * FROM bookmarks NATURAL JOIN player WHERE bookmarks.uid = ...;
 
 /* DELETE PLAYERS FROM WATCHLIST */
-DELETE FROM bookmarks WHERE bookmarks.pid = ...;
+DELETE FROM bookmarks WHERE bookmarks.pid = ... and bookmarks.uid = ...;

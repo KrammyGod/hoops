b4b5a2f9a186f0cd9f1bb0bcc456f262ba96fdbf
Brandon Ngo <okibrandon@hotmail.com>
2023-07-09 06:12:33 -0400

    added username show for homepage

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import styles from './page.module.css'
 import Leaderboards from './leaderboards'
+import { useAuth } from './auth'
 
 export default function Home() {
+  const { username } = useAuth()
   return (
     <main className={styles.main}>
-      <Leaderboards></Leaderboards>
+      <div className={styles.stack}>
+        <h3>{`Hi ${username}! Welcome to your dashboard.`}</h3>
+        <Leaderboards></Leaderboards>
+      </div>
+
     </main>
   )
 }

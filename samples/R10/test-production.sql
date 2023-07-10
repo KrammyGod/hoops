19f65f2f77d79d77a8108664b3b99bcc889ecbb8
saboorbakshi <saboorbak@icloud.com>
2023-07-10 09:27:40 -0400

    Completed R10-feature sample & production

--- samples/R10/test-production.sql
+++ samples/R10/test-production.sql
@@ -1,5 +1,9 @@
+---------- R10: Saboor ----------
 -- FILTER PLAYERS BY ONE OR MORE STATS --
-SELECT * 
+SELECT 
+    pid AS id, 
+    (firstName || ' ' || lastName) AS name,
+    games, points, assists, rebounds, season
 FROM Player 
 NATURAL JOIN PlayerStats 
 WHERE 
@@ -8,14 +12,21 @@ WHERE
     (points >= 1000 OR 1000 IS NULL) AND
     (games >= 75 OR 75 IS NULL) AND
     (season = NULL OR NULL IS NULL)
-LIMIT 10;
+ORDER BY name
+LIMIT 10
+OFFSET 0 * 10;
 
 -- FILTER TEAMS BY ONE OR MORE STATS --
-SELECT * 
+SELECT 
+    abbrev AS id, 
+    tname AS name, 
+    wins, losses, season
 FROM Team 
 NATURAL JOIN TeamStats
 WHERE 
     (wins >= 5 OR 5 IS NULL) AND
     (losses >= NULL OR NULL IS NULL) AND
     (season = 2000 OR 2000 IS NULL)
-LIMIT 10;
+ORDER BY name
+LIMIT 10
+OFFSET 0 * 10;

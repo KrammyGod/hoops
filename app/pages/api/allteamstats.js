eb73a850acffa5ca61c73eb00960693ace39853c
Sahl Bakshi <sahlaajazbakshi@gmail.com>
2023-07-10 22:29:52 -0400

    const error fixed

--- app/pages/api/allteamstats.js
+++ app/pages/api/allteamstats.js
@@ -2,7 +2,7 @@ import { getAllTeamStats } from "../../apis/stats.js";
 
 export default async function allteamstats(req, res) {
     try {
-        const page = (req.query.page ?? 1) - 1;
+        let page = (req.query.page ?? 1) - 1;
         page = (page < 0) ? 1 : page;
         const data = await getAllTeamStats(page);
         res.status(200).send(data);

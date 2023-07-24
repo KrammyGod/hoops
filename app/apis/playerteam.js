88ca0687092d999615c9de7711a7f3b33e73c44e
Sahl Bakshi <sahlaajazbakshi@gmail.com>
2023-07-23 23:06:42 -0400

    moidifed queries to support pages

--- app/apis/playerteam.js
+++ app/apis/playerteam.js
@@ -1,29 +1,33 @@
 import { query } from "@modules/pool.js";
 
 /* Returns all players matching name */
-async function searchPlayerName(name) {
+async function searchPlayerName(name, page) {
     const res = await query(`
         SELECT * FROM Player
         WHERE (firstName || ' ' || lastName) ILIKE
-        ('%' || $1 || '%') LIMIT 10
-        OFFSET 0 * 10;
-    `, [name]);
+        ('%' || $1 || '%') 
+        LIMIT 10
+        OFFSET $2 * 10;
+    `, [name, page]);
     return res.rows;
 }
 
 /* Returns all teams matching name */
-async function searchTeamName(name) {
+async function searchTeamName(name, page) {
     const res = await query(`
         SELECT * FROM Team
-        WHERE tName ILIKE ('%' || $1 || '%') LIMIT 10
-        OFFSET 0 * 10;
-    `, [name]);
+        WHERE tName ILIKE ('%' || $1 || '%') 
+        LIMIT 10
+        OFFSET $2 * 10;
+    `, [name, page]);
     return res.rows;
 }
 
 export const searchTeamByName = async (req, res) => {
     try {
-        const data = await searchTeamName(req.query.id);
+        let page = (req.query.page ?? 1) - 1;
+        page = (page < 0) ? 1 : page;
+        const data = await searchTeamName(req.query.id, page);
         res.status(200).json({ data });
     } catch (err) {
         res.status(501).json({ messages: err.stack });
@@ -32,7 +36,9 @@ export const searchTeamByName = async (req, res) => {
 
 export const searchPlayerByName = async (req, res) => {
     try {
-        const data = await searchPlayerName(req.query.id);
+        let page = (req.query.page ?? 1) - 1;
+        page = (page < 0) ? 1 : page;
+        const data = await searchPlayerName(req.query.id, page);
         res.status(200).json({ data });
     } catch (err) {
         res.status(501).json({ messages: err.stack });

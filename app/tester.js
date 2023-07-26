72cd1789202cc6a5a1a6287262c7361eb3da00e2
KrammyGod <shi.mark10@gmail.com>
2023-07-26 19:59:59 -0400

    Updated debugging to examine issue.

--- app/tester.js
+++ app/tester.js
@@ -1,17 +1,28 @@
-import { spawn } from 'child_process';
-import kill from 'tree-kill';
+import { exec, spawn } from 'child_process';
 
 // Windows thinks they're funny and uses different cmd
-const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+const win = /^win/.test(process.platform);
+const npm = win ? 'npm.cmd' : 'npm'
 
 // Spawn our backend for testing
 const api = spawn(npm, ['run', 'dev'], { detached: false });
+api.stdout.pipe(process.stdout);
+api.stderr.pipe(process.stderr);
 
 // Exit code of our tester will be the exit code of this process
 let exitCode = 0;
 function killMe() {
     console.log('My job is done, I think I will die now...');
     process.kill(exitCode);
+}
+
+const kill = (child, callback) => {
+    if (win) {
+        return exec(`taskkill /pid ${child.pid} /T /F`, callback);
+    }
+    child.once('close', callback)
+    child.once('exit', callback)
+    child.kill('SIGKILL');
 }
 
 api.once('spawn', () => {
@@ -24,8 +35,7 @@ api.once('spawn', () => {
     child.once('close', (code) => {
         console.log(`Test Completed with code ${code}`);
         exitCode = code ?? 0;
-        // We use this dependency to resolve platform issues
-        // For example, on windows, you have to use taskkill
-        kill(api.pid, 'SIGKILL', killMe);
+        // Call our self kill
+        kill(api, killMe);
     });
 });

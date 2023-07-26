09987e03ed63c212d5d4e1c226455211176ce24c
KrammyGod <shi.mark10@gmail.com>
2023-07-25 22:50:20 -0400

    Added AdminTable

---
+++ app/src/app/settings/AdminTable.tsx
@@ -0,0 +1,128 @@
+import './styles.css'
+import React from 'react';
+import { FcCancel } from "react-icons/fc";
+import { OverlayTrigger, Table, Tooltip } from "react-bootstrap";
+import { useTable, useSortBy, useFilters, usePagination, Column } from 'react-table';
+
+type Data = {
+    email: string;
+    uname: string;
+    urole: string;
+    deleteUser: boolean;
+};
+
+type DynamicTableProps = {
+    data: Data[];
+    onSubmit: (id: string, value: string) => void;
+    onIconClick: (row: Data) => void;
+};
+
+const AdminTable: React.FC<DynamicTableProps> = ({ data, onSubmit, onIconClick }) => {
+    const EmailCell = ({ value }: any) => {
+        return <span>{value}</span>
+    };
+
+    const keyDownWrapper = (e: any, id: string) => {
+        if (e.key === 'Enter') {
+            onSubmit(id, e.target.value);
+        }
+    };
+
+    const UsernameCell = ({ value, column }: any) => {
+        return (
+            <input
+                defaultValue={value}
+                onKeyDown={e => keyDownWrapper(e, column.id)}
+            />
+        );
+    };
+
+    const RoleCell = ({ value, column }: any) => {
+        return (
+            <input
+                defaultValue={value}
+                onKeyDown={e => keyDownWrapper(e, column.id)}
+            />
+        );
+    };
+
+    const IconCell = ({ row }: any) => {
+        return (
+            <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="delete">This will permanently delete this user.</Tooltip>}
+            >
+                <p style={{ margin: 0, fontSize: "24px", textAlign: "center", cursor: "pointer" }}>
+                    <FcCancel onClick={() => onIconClick(row.original)} />
+                </p>
+            </OverlayTrigger>
+        );
+    };
+
+    const columns: readonly Column<Data>[] = React.useMemo(
+        () => [
+            {
+                Header: 'Email',
+                accessor: 'email',
+                Cell: EmailCell
+            },
+            {
+                Header: 'Username',
+                accessor: 'uname',
+                Cell: UsernameCell
+            },
+            {
+                Header: 'Role',
+                accessor: 'urole',
+                Cell: RoleCell
+            },
+            {
+                Header: '',
+                accessor: 'deleteUser',
+                Cell: IconCell
+            },
+        ],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable<Data>({ columns, data }, useFilters, useSortBy, usePagination);
+
+    return (
+        <Table {...getTableProps()}>
+            <thead>
+                {headerGroups.map((headerGroup, index) => (
+                    <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()} key={column.id}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, index) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()} key={index}>
+                            {row.cells.map((cell) => (
+                                <td {...cell.getCellProps()} key={cell.column.id}>
+                                    {cell.render('Cell')}
+                                </td>
+                            ))}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </Table>
+    );
+};
+
+export default AdminTable;
--- app/src/app/settings/UserControl.tsx
+++ app/src/app/settings/UserControl.tsx
@@ -1,43 +1,43 @@
 'use client'
 
-import { OverlayTrigger, Tooltip, Toast, ToastContainer } from "react-bootstrap";
-import useSession from "@hooks/Auth"
-import BootstrapTable from 'react-bootstrap-table-next';
-// @ts-ignore
-import cellEditFactory, { Type } from 'react-bootstrap-table2-editor';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import { AiOutlineEdit } from "react-icons/ai";
-import { FcCancel } from "react-icons/fc"
-import { BsFillCheckCircleFill } from "react-icons/bs"
-import { MdOutlineError } from "react-icons/md"
-import { API } from "@/types/ApiRoute";
-import "../bookmarks/list.css"
+import '../bookmarks/list.css';
+import { API } from '@/types/ApiRoute';
+import { MdOutlineError } from 'react-icons/md';
+import { useEffect, useState } from 'react';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { Toast, ToastContainer } from 'react-bootstrap';
+import useSession from '@hooks/Auth';
 import styles from '../page.module.css';
-import React, { useEffect, useState } from "react";
+import AdminTable from './AdminTable';
 
 // @ts-ignore
-const NotifToast = ({ handleRemove, success, message }) => {
+type NotifToastParams = {
+    handleRemove: () => void;
+    success: boolean;
+    message: string;
+};
+const NotifToast = ({ handleRemove, success, message } : NotifToastParams) => {
     const [show, setShow] = useState(false);
     useEffect(() => {
-      setShow(true);
+        setShow(true);
     }, []);
     return (
-      <Toast
-        delay={7000}
-        show={show}
-        onClose={() => {
-          handleRemove();
-          setShow(false);
-        }}
-        autohide
-      >
-        <Toast.Header>
-            {success ? (<BsFillCheckCircleFill style={{ color: "green", fontSize: "20px" }} />) : (<MdOutlineError style={{ color: "red", fontSize: "20px"  }} />)}
-        </Toast.Header>
-        <Toast.Body>{message}</Toast.Body>
-      </Toast>
+        <Toast
+            delay={7000}
+            show={show}
+            onClose={() => {
+                handleRemove();
+                setShow(false);
+            }}
+            autohide
+        >
+            <Toast.Header>
+                {success ? (<BsFillCheckCircleFill style={{ color: 'green', fontSize: '20px' }} />) : (<MdOutlineError style={{ color: 'red', fontSize: '20px' }} />)}
+            </Toast.Header>
+            <Toast.Body>{message}</Toast.Body>
+        </Toast>
     );
-  };
+};
 
 const Control = () => {
     const { session } = useSession();
@@ -53,137 +53,132 @@ const Control = () => {
                 'Content-Type': 'application/json'
             }
         })
-        .then((res) => res.json())
-        .then((data) => setUsers(data.data.filter((user: any) => user.uid !== session?.user.id)))
-        .catch((err) => console.log(err));
-    }, [session])
+            .then((res) => res.json())
+            .then((data) => setUsers(data.data.filter((user: any) => user.uid !== session?.user.id)))
+            .catch((err) => console.log(err));
+    }, [session]);
 
-    const deleteUser = (uid: number, username: string) => {
-        fetch(`${API}/users/delete`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ uid })
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            setUsers(users.filter((it: any) => it.uid !== uid))
-            addToast({ id: uid, success: true, message: `Successfully deleted ${username}`, Component: NotifToast })
-        })
-        .catch((err) => {
-            console.log(err)
-            addToast({ id: uid, success: false, message: `Failed to delete ${username}`, Component: NotifToast })
-        });
-    }
+    // ALL COMMENTED CODE WILL HAVE TO BE REDONE
 
-    const columns = [{
-        dataField: 'email',
-        text: 'Email',
-        editable: false
-    }, {
-        dataField: 'uname',
-        text: 'Username',
-        headerFormatter: (column: any) => {
-            return <OverlayTrigger 
-                placement="bottom"
-                overlay={<Tooltip id="toolName">This column is editable. Click on a cell and press enter to save changes.</Tooltip>}
-            >
-                <p style={{ margin: 0 }}>
-                    {column.text}
-                    <AiOutlineEdit style={{ fontSize: "20px", color: "blue"}} />
-                </p>
-            </OverlayTrigger>
-    }}, {
-        dataField: 'urole',
-        text: 'Role',
-        headerFormatter: (column: any) => {
-            return <OverlayTrigger 
-                placement="bottom"
-                overlay={<Tooltip id="roleName">This column is editable. Click on a cell and press enter to save changes.</Tooltip>}
-            >
-                <p style={{ margin: 0 }}>
-                    {column.text}
-                    <AiOutlineEdit style={{ fontSize: "20px", color: "blue"}} />
-                </p>
-            </OverlayTrigger>
-        },
-        editor: {
-            type: Type.SELECT,
-            options: [{
-                value: "admin",
-                label: "admin"
-            }, {
-                value: "user",
-                label: "user"
-            }]
-        }
-    }, {
-        text: "",
-        dataField: 'deleteUser',
-        isDummyField: true,
-        editable: false,
-        headerStyle: { width: "50px" },
-        // @ts-ignore
-        formatter: (cellContent, row) => (
-            <OverlayTrigger 
-                placement="bottom"
-                overlay={<Tooltip id="delete">This will permanently delete this user.</Tooltip>}
-            >
-                <p style={{ margin: 0, fontSize: "24px", textAlign: "center", cursor: "pointer" }}>
-                    <FcCancel onClick={() => deleteUser(row.uid, row.uname)}/>
-                </p>
-            </OverlayTrigger>
-        )
-    }];
-    
-    // @ts-ignore
-    const handleTableChange = (type, { data, cellEdit: { rowId, dataField, newValue } }) => {
-        const result = data.map((row: any) => {
-            if (row.uid === rowId) {
-                fetch(`${API}/users/adminUpdate`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: dataField == "urole" ? 
-                        JSON.stringify({ uid: row.uid, username: row.uname, role: newValue }) :
-                        JSON.stringify({ uid: row.uid, username: newValue, role: row.urole }) 
-                })
-                .then((res) => res.json())
-                .then((data) => {
-                    addToast({ id: data.data.uid, success: true, message: `Successfully changed ${dataField} of ${row.email} to ${newValue}`, Component: NotifToast })
-                })
-                .catch((err: any) => {
-                    addToast({ id: row.uid, success: false, message: `Failed to modify ${dataField} of ${row.email}`, Component: NotifToast })
-                });
-                const newRow = {...row}
-                newRow[dataField] = newValue
-                return newRow
-            }
-            return row
-        });
-        setUsers(result)
-    }
+    // const deleteUser = (uid: number, username: string) => {
+    //     fetch(`${API}/users/delete`, {
+    //         method: 'DELETE',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({ uid })
+    //     })
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         setUsers(users.filter((it: any) => it.uid !== uid))
+    //         addToast({ id: uid, success: true, message: `Successfully deleted ${username}`, Component: NotifToast })
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //         addToast({ id: uid, success: false, message: `Failed to delete ${username}`, Component: NotifToast })
+    //     });
+    // }
+
+    // const columns = [{
+    //     dataField: 'email',
+    //     text: 'Email',
+    //     editable: false
+    // }, {
+    //     dataField: 'uname',
+    //     text: 'Username',
+    //     headerFormatter: (column: any) => {
+    //         return <OverlayTrigger 
+    //             placement='bottom'
+    //             overlay={<Tooltip id='toolName'>This column is editable. Click on a cell and press enter to save changes.</Tooltip>}
+    //         >
+    //             <p style={{ margin: 0 }}>
+    //                 {column.text}
+    //                 <AiOutlineEdit style={{ fontSize: '20px', color: 'blue'}} />
+    //             </p>
+    //         </OverlayTrigger>
+    // }}, {
+    //     dataField: 'urole',
+    //     text: 'Role',
+    //     headerFormatter: (column: any) => {
+    //         return <OverlayTrigger 
+    //             placement='bottom'
+    //             overlay={<Tooltip id='roleName'>This column is editable. Click on a cell and press enter to save changes.</Tooltip>}
+    //         >
+    //             <p style={{ margin: 0 }}>
+    //                 {column.text}
+    //                 <AiOutlineEdit style={{ fontSize: '20px', color: 'blue'}} />
+    //             </p>
+    //         </OverlayTrigger>
+    //     },
+    //     editor: {
+    //         type: Type.SELECT,
+    //         options: [{
+    //             value: 'admin',
+    //             label: 'admin'
+    //         }, {
+    //             value: 'user',
+    //             label: 'user'
+    //         }]
+    //     }
+    // }, {
+    //     text: '',
+    //     dataField: 'deleteUser',
+    //     isDummyField: true,
+    //     editable: false,
+    //     headerStyle: { width: '50px' },
+    //     // @ts-ignore
+    //     formatter: (cellContent, row) => (
+    //         <OverlayTrigger 
+    //             placement='bottom'
+    //             overlay={<Tooltip id='delete'>This will permanently delete this user.</Tooltip>}
+    //         >
+    //             <p style={{ margin: 0, fontSize: '24px', textAlign: 'center', cursor: 'pointer' }}>
+    //                 <FcCancel onClick={() => deleteUser(row.uid, row.uname)}/>
+    //             </p>
+    //         </OverlayTrigger>
+    //     )
+    // }];
+
+    // // @ts-ignore
+    // const handleTableChange = (type, { data, cellEdit: { rowId, dataField, newValue } }) => {
+    //     const result = data.map((row: any) => {
+    //         if (row.uid === rowId) {
+    //             fetch(`${API}/users/adminUpdate`, {
+    //                 method: 'POST',
+    //                 headers: {
+    //                     'Content-Type': 'application/json'
+    //                 },
+    //                 body: dataField == 'urole' ? 
+    //                     JSON.stringify({ uid: row.uid, username: row.uname, role: newValue }) :
+    //                     JSON.stringify({ uid: row.uid, username: newValue, role: row.urole }) 
+    //             })
+    //             .then((res) => res.json())
+    //             .then((data) => {
+    //                 addToast({ id: data.data.uid, success: true, message: `Successfully changed ${dataField} of ${row.email} to ${newValue}`, Component: NotifToast })
+    //             })
+    //             .catch((err: any) => {
+    //                 addToast({ id: row.uid, success: false, message: `Failed to modify ${dataField} of ${row.email}`, Component: NotifToast })
+    //             });
+    //             const newRow = {...row}
+    //             newRow[dataField] = newValue
+    //             return newRow
+    //         }
+    //         return row
+    //     });
+    //     setUsers(result)
+    // }
     return (
         <>
-            <ToastContainer position="top-end">
-                {toasts.map(({id, Component, success, message}, index) => (
-                    <Component key={id} success={success} message={message} handleRemove={() => removeToast(id)}/>
+            <ToastContainer position='top-end'>
+                {toasts.map(({ id, Component, success, message }, index) => (
+                    <Component key={id} success={success} message={message} handleRemove={() => removeToast(id)} />
                 ))}
             </ToastContainer>
             <div className={styles.settingsContainer}>
-                <BootstrapTable
-                    hover
-                    rowStyle={{ wordBreak: 'break-all' }}
-                    keyField="uid"
-                    columns={ columns }
-                    data={ users }
-                    onTableChange={ handleTableChange }
-                    cellEdit = {cellEditFactory({ mode: 'click' })}
-                    remote={{
-                        cellEdit: true
-                    }}
+                <AdminTable
+                    data={users}
+                    onSubmit={(id, val) => { console.log(id, val) }}
+                    onIconClick={() => {/*call delete user here*/ }}
                 />
             </div>
         </>
